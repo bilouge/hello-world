729aaabd4172f02a9c31f3ca4b0283528758a469
bilouge <41525881+bilouge@users.noreply.github.com>
2018-07-21 19:32:36 -0400

    Add files via upload

--- test.js
+++ test.js
@@ -13,19 +13,19 @@ function updateClickCount() {
 }
 // Var for gloves upgrades
 var glovesUpgrades = 0;
-document.getElementById("glovesUpgradesCount").innerHTML = glovesUpgrades;
+document.getElementById("glovesUpgradesCount").innerHTML = `LVL ${glovesUpgrades}`;
 // Multiplicator of gloves upgrades
 var glovesMulti = 1;
 // Add 1 per click to the gloves upgrades
 
-document.getElementById("glovesCost").innerHTML = "("+ glovesMulti + ")";
+document.getElementById("glovesCost").innerHTML = `(${glovesMulti})`;
 function updateGlovesCount() {
     if (numberOfGrapes >= glovesMulti) {
         glovesUpgrades++;
         grapesMulti++;
         numberOfGrapes = numberOfGrapes - glovesMulti;
         glovesMulti = glovesMulti * 2;
-        document.getElementById("glovesUpgradesCount").innerHTML = glovesUpgrades;
+        document.getElementById("glovesUpgradesCount").innerHTML = `LVL ${glovesUpgrades}`;
         document.getElementById("grapeNumber").innerHTML = numberOfGrapes;
         document.getElementById("glovesCost").innerHTML = "("+ glovesMulti + ")";
        grapesClick = Math.pow(2,grapesMulti); document.getElementById("numberOfGrapesPerClick").innerHTML = grapesClick;
